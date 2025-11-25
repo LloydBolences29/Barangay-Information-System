c61bb1b35bd49a6912afd3dd032ce635423b7398
Lloyd <lbolences05@gmail.com>
2025-11-25 17:17:37 +0800

    pushed

--- bim-frontend/src/component/SecretaryComponent/SecretaryDashboard.jsx
+++ bim-frontend/src/component/SecretaryComponent/SecretaryDashboard.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./SecretaryDashboard.css";
 
 const SecretaryDashboard = () => {
   return (
     <>
-    <div id="main-secretary-dashboard">
+      <div id="main-secretary-dashboard">
         <div id="welcome-banner">
-            <h1>Welcome to your dashboard</h1>
+          <h1>Welcome to your dashboard</h1>
         </div>
-    </div>
-      
+        <div className="dashboard-container">
+          <div className="dashboard-wrapper">
+            <h1>this is the container for your stats</h1>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SecretaryDashboard
+export default SecretaryDashboard;
